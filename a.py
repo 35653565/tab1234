f6965bc272656d29680c15845934d560593d9f09
Adam <mckillopadam@gmail.com>
2021-05-07 10:55:39 -0400

    dfldajf;ldakjf

--- a.py
+++ a.py
@@ -2,9 +2,16 @@ import bs4 as bs
 import requests
 
 def get_sp500_weights():
-    resp = requests.get('https://www.slickcharts.com/sp500')
+
+    # get data from website, parse with beautiful soup
+    url = 'https://www.slickcharts.com/sp500'
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
+    resp = requests.get(url, headers=headers)
     soup = bs.BeautifulSoup(resp.text, features="lxml")
-    table = soup.find('table', {'class': 'table table-hover table-borderless table-sm'})
+    print(soup)
+
+    table = soup.find('table', {'<class': 'table table-hover table-borderless table-sm>'})
+    print(table)
     sp500_weights = []
     for row in table.findAll('tr')[1:]:
         sp500_weight = row.findAll('td')[3].text
